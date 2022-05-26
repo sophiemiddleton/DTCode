81f2554807d93b2b2ead10771dbd60181cee30ed
sophiemiddleton <>
2022-05-26 11:23:02 -0500

    added comments

--- data_analysis.py
+++ data_analysis.py
@@ -1,11 +1,14 @@
 # Analysis of the data taken from the REM 500 Neutron Survey Meter
 
+# Authors: Leo Borrel, Sophie Middleton
+
 import matplotlib.pyplot as plt
 import csv
 
 time = []
 counts = []
 
+# read the raw data and convert time
 with open('raw_data.txt') as data_file:
     for line in data_file:
         if len(line) == 16:
@@ -14,7 +17,7 @@ with open('raw_data.txt') as data_file:
             time.append(int(date[6:8]) + 60*int(date[3:5]) + 3600*int(date[0:2]))
             counts.append(value)
 
-# Export data to csv
+# Export time data to csv
 csv_file = open('data.csv', 'w')
 header = ['time', 'counts']
 
@@ -27,7 +30,7 @@ with open('data.csv', 'w') as csv_file:
         writer.writerow(row)
 
 
-# plots
+# Make plots of counts over time
 
 plt.figure()
 plt.plot(time, counts)
@@ -38,19 +41,20 @@ plt.ylabel('Counts')
 
 
 # Plot of the data from each channel
-
 channel = []
 
 with open('channel_data.txt') as channel_file:
     for line in channel_file:
         channel.append(int(line[0:5]))
 
+# access quality factor from text file
 QF = []
 
 with open('QF.txt') as QF_file:
     for line in QF_file:
         QF.append(float(line[0:-1]))
 
+# translate and print:
 rad = 0
 for i in range(5,255):
     rad += 100 * i * channel[i] / 20
@@ -66,7 +70,7 @@ rem = rem / time[-1]
 print('rad: ', rad, ' urad/h')
 print('rem: ', rem, ' urem/h')
 
-# plot
+# final plot of channel output:
 
 plt.figure()
 plt.bar(range(256),channel)
